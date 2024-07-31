e8d5cda533667c79875c952370d6ef2cf1380e1f
jaum <juaun1981@gmail.com>
2024-07-31 10:33:20 -0300

    implementacao do Enum OrderStatus e sua devida associacao na classe Order

--- project/src/main/java/com/example/project/config/TestConfig.java
+++ project/src/main/java/com/example/project/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.project.entities.Order;
 import com.example.project.entities.User;
+import com.example.project.entities.enums.OrderStatus;
 import com.example.project.repositories.OrderRepository;
 import com.example.project.repositories.UserRepository;
 
@@ -32,9 +33,9 @@ public class TestConfig implements CommandLineRunner {// dataBase seeding
         User user2 = new User(null, "maria", "maria@email.com", "87654321", "321");
 
         // formato ISO8601
-        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1); //assiociacao entre os objetos ", user1"
-        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1); //assiociacao entre os objetos ", user1"
+        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAINTING_PAYMENT, user2);
+        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, user1);
 
         // salvar no banco de dados:
         userRepository.saveAll(Arrays.asList(user1, user2)); // salva em uma lista
--- project/src/main/java/com/example/project/entities/Order.java
+++ project/src/main/java/com/example/project/entities/Order.java
@@ -3,6 +3,7 @@ package com.example.project.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.example.project.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -27,6 +28,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus; // para dizer que dentro do banco de dados 'e um inteiro'
+
     @ManyToOne // instrui o jpa prar transformar em uma chave estrangeira
     @JoinColumn(name = "clientId") // nome da chave estrangeira
     private User client;
@@ -34,9 +37,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -62,6 +66,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
