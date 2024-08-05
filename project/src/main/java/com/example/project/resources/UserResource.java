d8de835221da7d411c0c9f85f1b7af369a1f580f
jaum <juaun1981@gmail.com>
2024-08-05 15:59:14 -0300

    insercao de user atraves do 'metodo' post

--- project/src/main/java/com/example/project/resources/UserResource.java
+++ project/src/main/java/com/example/project/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.example.project.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.project.entities.User;
 import com.example.project.services.UserService;
@@ -32,5 +36,12 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) { // para considerar como parametro
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping // recebe o metodo post do http
+    public ResponseEntity<User> insert(@RequestBody User user) { // anotation para desserializar json para obj User
+        service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
--- project/src/main/java/com/example/project/services/UserService.java
+++ project/src/main/java/com/example/project/services/UserService.java
@@ -25,4 +25,8 @@ public class UserService {
         Optional<User> user = repository.findById(id);
         return user.get();
     }
+
+    public User insert(User user) {
+        return repository.save(user);
+    }
 }
