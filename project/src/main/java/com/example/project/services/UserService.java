6b2d9489885cfe2e8d07e9cc4f8c98bcc21c90f9
jaum <juaun1981@gmail.com>
2024-08-06 12:21:57 -0300

    implementacao do tratamento de excecoes no ExceptionHandler (delete)

--- project/src/main/java/com/example/project/services/UserService.java
+++ project/src/main/java/com/example/project/services/UserService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.project.entities.User;
